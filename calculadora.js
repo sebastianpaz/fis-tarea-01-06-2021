fbf6f5eb80fad4189de76434d8838a1cde67c9ca
soledadlinares <sollinar@gmail.com>
2021-06-01 20:20:22 -0300

    algo

---
+++ calculadora.js
@@ -0,0 +1,20 @@
+class Calculadora{
+
+    Calculadora(){};
+
+    suma (a, b){
+        return a+b;
+    }
+
+    resta(a, b){
+        return a-b;
+    }
+
+    multiplicacion(a, b){
+        return a*b;
+    }
+
+    division(a, b){
+        return a/b;
+    }
+} export default Calculadora;
