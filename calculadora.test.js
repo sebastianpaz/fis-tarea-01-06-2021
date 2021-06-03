cc24f2a135be740467b83a585c81d6db21b3cb86
Carla R <csrodb@gmail.com>
2021-06-03 17:12:35 -0300

    agrego test multiplicacion

--- calculadora.test.js
+++ calculadora.test.js
@@ -3,3 +3,7 @@ import Calculadora from './calculadora';
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('product 0 * 20  equals 0', () => {
+    expect(multiplicacion(0, 20)).toBe(0);
+});
